95293632a3f9b40487060e482bdf6e34ae8ea905
dariofusco <fdario@hotmail.it>
2023-10-17 15:41:16 +0200

    fix ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->paginate();
+        $projects = Project::with('type', 'technologies')->get();
         return response()->json([
             'success' => true,
             'results' => $projects
